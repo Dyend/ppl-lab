46054dd66a60057661de12274ab620c3e6515964
Jose <alonso__96@hotmail.es>
2021-09-22 11:40:17 -0300

    agregada interfaz para funcion y restricciones

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+import json
+import webbrowser
+import os
 
 
 """ Clase para representar una restriccion por separado su ecuacion, operador y resultado"""
@@ -10,6 +13,21 @@ class Restriccion():
         self.ecuacion = ecuacion
         self.operador = operador
         self.resultado = resultado
+
+    def funcion(self, x):
+        num_x = self.ecuacion[0]
+        num_y = self.ecuacion[1]
+        return (self.resultado - num_x * x ) / num_y
+""" Clase para guardar los datos de una funcion objetivo dinamica, es decir ingresada por el usuario"""
+class FuncionObjetivo():
+    def __init__(self, f_obj_x, f_obj_y, suma_resta):
+        self.f_obj_x = f_obj_x
+        self.f_obj_y = f_obj_y
+        self.suma_resta = suma_resta
+    def calcular(self, x, y):
+        if self.suma_resta != "suma":
+            return self.f_obj_x *x - self.f_obj_y * y
+        return self.f_obj_x * x + self.f_obj_y * y
 
 """ obtiene las intersecciones entre 2 restricciones"""
 def get_intersection(restricciones):
@@ -34,16 +52,17 @@ def has_solution(matriz):
     return False
 
 # Funcion objetivo
-def funcion_objetivo(x, y):
-    return 30 * x + 50 * y
+def funcion_objetivo(f_obj_x, f_obj_y, x , y):
+    return f_obj_x * x + f_obj_y * y
 
 # Curva de nivel de la funcion objetivo
-def curva_nivel():
+def curva_nivel(f_objetivo):
     x = np.linspace(0, 100, 100)
     y = np.linspace(0, 100, 100)
 
     X, Y = np.meshgrid(x, y)
-    Z = funcion_objetivo(X, Y)
+    
+    Z = f_objetivo.calcular(X,Y)
     plt.contour(X, Y, Z, levels=80)
     plt.show()
 
@@ -111,11 +130,11 @@ def solucion_factible(restricciones, vertices):
     recibe por parametro los vertices y la variable maximo
     si se quiere maximizar debe ser True, para minimizar debe ser False"""
 
-def obtener_optimo(vertices, maximo):
+def obtener_optimo(vertices, maximo, f_objetivo):
     best_resultado = 0
     best = None
     for vertice in vertices:
-        resultado = funcion_objetivo(vertice[0], vertice[1])
+        resultado = f_objetivo.calcular(vertice[0], vertice[1])
         if maximo:
             if resultado >= best_resultado:
                 best = vertice
@@ -161,47 +180,77 @@ def siguiente_vertice(restriccion, copy_vertices, old_vertice):
                 return vertice
     return old_vertice
 
-if __name__ == "__main__":
-    curva_nivel()
+# retorna los datos del problema
+def datos():
+    """ se lee el json con los datos"""
+
+    with open("json.txt") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
+
+    f_obj_x = int(jsonObject['f_obj_x'])
+    f_obj_y = int(jsonObject['f_obj_y'])
+    sum_resta = jsonObject['sum_resta']
+    min_max = jsonObject['min_max']
+    if min_max == "maximizar":
+        min_max = True
+    else:
+        min_max= False
+    contador = 1
+    restricciones = []
+    # buscando todas las restriccioens en el json
+    while 'x_restriccion_' + str(contador) in jsonObject.keys():
+        restriccion_x = int(jsonObject['x_restriccion_' + str(contador)])
+        restriccion_y = int(jsonObject['y_restriccion_' + str(contador)])
+        sum_resta_r = jsonObject['sum_resta_' + str(contador)]
+        if sum_resta_r == "-":
+            restriccion_y = restriccion_y * -1
+        operadores_r = jsonObject['operadores_' + str(contador)]
+        resultado_r = int(jsonObject['resultado_' + str(contador)])
+        restriccion_x = Restriccion([restriccion_x, restriccion_y], operadores_r, resultado_r)
+        restricciones.append(restriccion_x)
+        contador += 1
+
+    f_objetivo = FuncionObjetivo(f_obj_x, f_obj_y, sum_resta)
+    datos = {
+        'f_objetivo': f_objetivo,
+        'restricciones': restricciones,
+        'min_max': min_max
+    }
+
+    return datos
+
+def solve(datos):
+    
+    curva_nivel(datos['f_objetivo'])
     
     """ valores que tendra x en el grafico """
     x = np.arange(-100, 100)
 
-    """ Las restricciones deben tener 2 formatos, el de objeto y el de funcion despejada para poder graficarla"""
-    # Restricciones escritas en vectores y como funciones
-    # -x + y <= 11
-    restriccion_1 = Restriccion([-1, 1], "<=", 11)
-    y1 = 11 + x
     
-    restriccion_2 = Restriccion([1, 1], "<=", 27)
-    y2 = 27 - x
+    for restriccion in datos['restricciones']:
+        plt.plot(x, restriccion.funcion(x), '-', linewidth=2, color='b')
 
-    restriccion_3 = Restriccion([2, 5], "<=", 90)
-    y3 = (90 - 2 *x) / 5
-    
-    restriccion_5 = Restriccion([1, 0], ">=", 0)
+    restriccion_x = Restriccion([1, 0], ">=", 0)
+    restriccion_y = Restriccion([0, 1], ">=", 0)
+    datos['restricciones'].append(restriccion_x)
+    datos['restricciones'].append(restriccion_y)
 
-    x1 = 0 
-    restriccion_6 = Restriccion([0, 1], ">=", 0)
-
+    x1 = 0
     y4 = 0
 
-    restricciones = [restriccion_1, restriccion_2, restriccion_3, restriccion_5, restriccion_6]
-    plt.plot(x, y1, '-', linewidth=2, color='b')
-    plt.plot(x, y2, '-', linewidth=2, color='g')
-    plt.plot(x, y3, '-', linewidth=2, color='g')
-
-
+    # graficos verticales y horizontales
     plt.axhline(y = y4, linewidth=2, color='c')
     plt.axvline(x = x1, linewidth=2, color='r')
     plt.grid()
-    vertices = get_vertices(restricciones)
-    vertices = solucion_factible(restricciones, vertices)
+
+    vertices = get_vertices(datos['restricciones'])
+    vertices = solucion_factible(datos['restricciones'], vertices)
     if len(vertices) == 0:
         print('El espacio factible es vacio')
     elif len(vertices) > 2:
         """ Se verifica si por lo menos existen 3 vertices que es lo minimo para formar un espacio cerrado"""
-        cerrado = espacio_cerrado(restricciones, vertices)
+        cerrado = espacio_cerrado(datos['restricciones'], vertices)
         if cerrado:
             print(f'El conjunto de posibles soluciones es {vertices}')
             graficar_vertices(vertices)
@@ -209,8 +258,7 @@ if __name__ == "__main__":
             # Vertices ordenados con la coordenada X de menor a mayor
             vertices = sorted(vertices, key=lambda tup: (tup[0], tup[1]))
             graficar_espacio(vertices)
-            """ Si se desea minimzar cambiar por false"""
-            optimo = obtener_optimo(vertices, True)
+            optimo = obtener_optimo(vertices, datos['min_max'], datos['f_objetivo'])
             print(f'El valor optimo es en el vertice ({optimo[0]}, {optimo[1]})')
             ax = plt.subplot()
             ax.set_ylim(bottom=0)
@@ -231,3 +279,10 @@ if __name__ == "__main__":
             plt.show()
         else:
             print('El espacio factible es abierto')
+
+if __name__ == "__main__":
+    webbrowser.open('file://' + os.path.realpath('index.html'))
+    input('Presione enter si ya ingreso los datos: ')
+    solve(datos())
+    
+
